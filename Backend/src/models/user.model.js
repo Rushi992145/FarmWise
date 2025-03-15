65fca22b2ab80dd3404c19951fc7abd9ca1a30e2
rushi992145 <rushikeshiray99@gmail.com>
2025-03-15 14:11:00 +0530

    backend setup

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+    userType : {
+        type: String,
+        default: 'user'
+    },
+    profilePic: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
