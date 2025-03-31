47c4a859e6fd8bc577e7de9466d4baf03eb52a51
Anuj5504 <anujjadhav0505@gmail.com>
2025-03-31 21:58:03 +0530

    resolved login error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,11 +5,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
-
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
