1e1a7f8dd2af30e61ce75d1b3ff7002f210ec4ab
rushi992145 <rushikeshiray99@gmail.com>
2025-04-21 15:59:40 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
